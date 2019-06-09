9bd8828b883de197cfcc2b693b87c69c11f7a055
gr8fuldwg <gr8fuldwg@gmail.com>
2019-06-08 20:20:16 -0400

    added react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Route, NavLink, BrowserRouter as Router, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <NavLink className="active" to="/"/>
+                    Home
+                </li>
+            </ul>
+            <hr />
+        </div>
+    </Router>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+ReactDOM.render(routing, document.getElementById('root'));
+
+

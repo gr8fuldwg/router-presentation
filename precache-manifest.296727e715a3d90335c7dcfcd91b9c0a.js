55bf0ca16286e38f57c3611b97b71b43ae942d99
gr8fuldwg <gr8fuldwg@gmail.com>
2019-06-19 21:51:20 -0400

    Updates

---
+++ precache-manifest.296727e715a3d90335c7dcfcd91b9c0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9224c9fc29965b63e0bccf4d8096fed",
+    "url": "/router-presentation/index.html"
+  },
+  {
+    "revision": "90640936005af76784a6",
+    "url": "/router-presentation/static/css/main.0c42a7db.chunk.css"
+  },
+  {
+    "revision": "f103cc6bf96601c0f73b",
+    "url": "/router-presentation/static/js/2.a4b9ca59.chunk.js"
+  },
+  {
+    "revision": "90640936005af76784a6",
+    "url": "/router-presentation/static/js/main.cf2428e9.chunk.js"
+  },
+  {
+    "revision": "12663ffb12be43c2f385",
+    "url": "/router-presentation/static/js/runtime~main.a3609b3c.js"
+  },
+  {
+    "revision": "56b44db592c9f1cd4a07f673644eb9a3",
+    "url": "/router-presentation/static/media/badcat.56b44db5.jpg"
+  },
+  {
+    "revision": "75f87f11281995e8367775e71cf48788",
+    "url": "/router-presentation/static/media/cat.75f87f11.jpg"
+  },
+  {
+    "revision": "e781556b512c7fbd95834b31ce67911e",
+    "url": "/router-presentation/static/media/waterdogs.e781556b.jpg"
+  },
+  {
+    "revision": "722038ffaaca53ecb04ad10a800fcc6f",
+    "url": "/router-presentation/static/media/waterdogs2.722038ff.jpg"
+  }
+]);
